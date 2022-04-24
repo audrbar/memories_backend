62c1d907757d868f056425f28f7762e35085dd03
audrbar <audrbar@gmail.com>
2022-04-24 21:06:07 +0300

    renewed

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
@@ -18,7 +19,6 @@ app.get('/', (req, res) => {
 });
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
-
 const PORT = process.env.PORT || 5500;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
