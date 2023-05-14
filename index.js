4483eeb32f89161616ea8322e0e364ce182cfa99
audrbar <audrbar@gmail.com>
2023-05-14 15:37:46 +0300

    fixes

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get('/', (req, res) => {
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5500;
 
+// const mongoose = require("mongoose");
+
+// mongoose.set('strictQuery', false);
+
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
