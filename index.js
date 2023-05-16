c883a2574c0527c786403e9414f50a0a729d63ef
audrbar <audrbar@gmail.com>
2023-05-16 20:29:56 +0300

    fixed cors

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true
-// }));
 
 app.use('/posts', postRoutes);
 
